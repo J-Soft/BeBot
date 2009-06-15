d958a95cd605e192158203dfe6bf071dbc32c248
Temar <chris_smith96@hotmail.com>
2009-06-15 04:51:07 +0100

    Added support for allowing <botname> to be used in prefix variables, also added

--- Sources/MySQL.php
+++ Sources/MySQL.php
@@ -71,6 +71,7 @@ class MySQL
 		}
 		else
 		{
+			$master_tablename = str_ireplace("<botname>", strtolower($botname), $master_tablename);
 			$this -> master_tablename = $master_tablename;
 		}
 
@@ -80,8 +81,13 @@ class MySQL
 		}
 		else
 		{
+			$table_prefix = str_ireplace("<botname>", strtolower($botname), $table_prefix);
 			$this -> table_prefix = $table_prefix;
 		}
+		if($nounderscore)
+			$this -> underscore = "";
+		else
+			$this -> underscore = "_";
 
 		$this -> connect(true);
 
@@ -284,7 +290,7 @@ class MySQL
 			}
 			else
 			{
-				$tablename = $this -> table_prefix . "_" . $table;
+				$tablename = $this -> table_prefix . $this -> underscore . $table;
 			}
 			$this -> query("INSERT INTO " . $this -> master_tablename . " (internal_name, prefix, use_prefix) VALUES ('" . $table . "', '" . $this -> table_prefix . "', 'true')");
 		}
@@ -293,7 +299,7 @@ class MySQL
 			// entry exists, create the correct tablename:
 			if ($name[0][2] == 'true' && !empty($this -> table_prefix))
 			{
-				$tablename = $name[0][1] . "_" . $table;
+				$tablename = $name[0][1] . $this -> underscore . $table;
 			}
 			else
 			{
@@ -334,7 +340,7 @@ class MySQL
 			if (((strtolower($use_prefix) == 'true') || ($use_prefix === true)) && !empty($this -> table_prefix))
 			{
 				$prefix = $this -> table_prefix;
-				$tablename = $prefix . "_" . $table;
+				$tablename = $prefix . $this -> underscore . $table;
 			}
 			else
 			{
@@ -348,7 +354,7 @@ class MySQL
 			// entry exists, create the correct tablename:
 			if ($name[0][2] == 'true' && !empty($this -> table_prefix))
 			{
-				$tablename = $name[0][1] . "_" . $table;
+				$tablename = $name[0][1] . $this -> underscore . $table;
 			}
 			else
 			{
