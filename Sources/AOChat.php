78a1cf0fcbcd255d72c374cfd27659155b5e2ef5
Thomas Juberg <Thomas.Juberg@Gmail.com>
2009-07-08 22:14:33 +0200

    - RPC_UNIVERSE_ACCOUNT is not defined, does not appear to be used and is causing PHP notices. Commenting out

--- Sources/AOChat.php
+++ Sources/AOChat.php
@@ -2098,7 +2098,7 @@ $GLOBALS["aochat-rpcpacketmap"] = array(
 "out" => array(
 	(string)RPC_UNIVERSE_INIT				=> array("name"=>"Login Init",					"args"=>"SSI"),
 	(string)RPC_UNIVERSE_ANSWERCHALLENGE	=> array("name"=>"Login Answer Challenge",		"args"=>"S"),
-	(string)RPC_UNIVERSE_ACCOUNT			=> array("name"=>"Login Player Account",		"args"=>"II"),
+//	(string)RPC_UNIVERSE_ACCOUNT			=> array("name"=>"Login Player Account",		"args"=>"II"),
 	(string)RPC_TERRITORY_INIT				=> array("name"=>"Player Init",					"args"=>"II"),
 	(string)RPC_TERRITORY_STARTUP			=> array("name"=>"Player Startup",				"args"=>""),
 	(string)RPC_TERRITORY_LOGINCHARACTER	=> array("name"=>"Login Character",				"args"=>"ISS"))
