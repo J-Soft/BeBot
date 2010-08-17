fd9395c8ce9cfd3362d46b28c4b9d4a74bb4a04e
Thomas Juberg <Thomas.Juberg@Gmail.com>
2010-08-17 20:23:53 +0200

    - Fix a brainfart

--- Sources/Bot.php
+++ Sources/Bot.php
@@ -502,10 +502,10 @@ class Bot
 	/*
 	sends a message over IRC if it's enabled and connected
 	*/
+	function send_irc($prefix, $name, $msg)	
+	{
 //		if (isset($this -> irc) && $this -> exists_module("irc"))
 		if ($this -> exists_module("irc"))
-	{
-		if (isset($this->irc) && $this->exists_module("irc"))
 		{
 			if ($this->core("settings")->get("Irc", "Connected"))
 			{
