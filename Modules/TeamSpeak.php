df24175e87f941eccaf82d9dedae2f271d7d85f6
bitnykk <beatnykk@gmx.fr>
2021-04-15 20:13:38 +0200

    Making TS module compatible with 3.0

--- Modules/TeamSpeak.php
+++ Modules/TeamSpeak.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* TeamSpeak.php - Adds TeamSpeak support to the bot
+* TeamSpeak.php - Adds TeamSpeak3 support to the bot
 * Bitnykk module from available Bebot archives
 * Thanks Teknologist & AoSpeak to accept being default server
 * BeBot - An Anarchy Online & Age of Conan Chat Automaton
@@ -45,11 +45,14 @@ class Teamspeak Extends BaseActiveModule
 		$this -> help['description'] = "Shows information about teamspeak.";
 		$this -> help['command']['ts'] = "See description";
 		
-		$this -> bot -> core("settings") -> create("Teamspeak", "tsip", "141.157.197.21", "What is TS server IP ?");		
-		$this -> bot -> core("settings") -> create("Teamspeak", "tsqp", "10011", "What is TS Query Port ?");		
-		$this -> bot -> core("settings") -> create("Teamspeak", "tssp", "9987", "What is TS Server Port ?");		
-		$this -> bot -> core("settings") -> create("Teamspeak", "tssn", "AoSpeak", "What is TS Server Name ?");		
+		$this -> bot -> core("settings") -> create("Teamspeak", "tsip", "141.157.197.21", "What is TS server IP ?");
+		$this -> bot -> core("settings") -> create("Teamspeak", "tsqp", "10011", "What is TS Query Port ?");
+		$this -> bot -> core("settings") -> create("Teamspeak", "tssp", "9987", "What is TS Server Port ?");
+		$this -> bot -> core("settings") -> create("Teamspeak", "tssn", "AoSpeak", "What is TS Server Name ?");
 		$this -> bot -> core("settings") -> create("Teamspeak", "tsdh", "voice.aospeak.com", "What is TS Display Host ?");
+		$this -> bot -> core("settings") -> create("Teamspeak", "tssi", "1", "What is TS Server Id ?");
+		$this -> bot -> core("settings") -> create("Teamspeak", "tsln", "", "What is TS Login Name ?");
+		$this -> bot -> core("settings") -> create("Teamspeak", "tslp", "", "What is TS Login Password ?");
 		
 	}
 
@@ -79,126 +82,45 @@ class Teamspeak Extends BaseActiveModule
 			}
 		}
 		if ($connection1) {
-			fputs($connection1,"sel ".$this->bot->core("settings")->get("Teamspeak","tssp")."\n");
-			fputs($connection1,"si\n");
+			echo " connected ";
+			if ($this->bot->core("settings")->get("Teamspeak","tsln")) {
+				fputs($connection1, "login client_login_name=".$this->bot->core("settings")->get("Teamspeak","tsln")." client_login_password=".$this->bot->core("settings")->get("Teamspeak","tslp")."\n");
+			}
+			fputs($connection1, "use ".$this->bot->core("settings")->get("Teamspeak","tssi")."\n");			
+			fputs($connection1, "channellist\n");	
+			fputs($connection1, "clientlist\n");	
 			fputs($connection1,"quit\n");
-
 			while(!feof($connection1)){
 				$infolines.=fgets($connection1,1024);
 			}
-			$infolines = str_replace("[TS]","",$infolines);
-			$infolines = str_replace("OK","",$infolines);
-			$infolines = trim($infolines);
-
-			// Servername, here but not used, uncomment if you want to use it
-			$name=substr($infolines,$this -> indexOf($infolines,"server_name="),strlen($infolines));
-			$name=substr($name,0,$this -> indexOf($name,"server_platform=")-strlen("server_platform="));
-
-			// Connected Users
-			$user=substr($infolines,$this -> indexOf($infolines,"server_currentusers="),strlen($infolines));
-			$user=substr($user,0,$this -> indexOf($user,"server_currentchannels=")-strlen("server_currentchannels="));
-
-			// Server Max Users
-			$maxusers=substr($infolines, $this -> indexOf($infolines,"server_maxusers="),strlen($infolines));
-			$maxusers=substr($maxusers,0,$this -> indexOf($maxusers,"server_allow_codec_celp51=")-strlen("server_allow_codec_celp51="));
-
-			// Number of channels, here but not used, uncomment if you want to use it
-			$numchan=substr($infolines,$this -> indexOf($infolines,"server_currentchannels="),strlen($infolines));
-			$numchan=substr($numchan,0,$this -> indexOf($numchan,"server_bwinlastsec=")-strlen("server_bwinlastsec="));
-
+			$infolines = preg_replace( "/\r|\n/", "",$infolines);
+			preg_match_all("/cid=([0-9]+) pid=(?:[0-9]+) channel_order=(?:[0-9]+) channel_name=([^ ]+) total_clients=([0-9]+) channel_needed_subscribe_power=(?:[0-9]+)/i",$infolines,$channels);
+			$chtot = count($channels[0]);
+			preg_match_all("/clid=(?:[0-9]+) cid=([0-9]+) client_database_id=(?:[0-9]+) client_nickname=([^ ]+) client_type=(?:[0-9]+)/i",$infolines,$users);
+			$cltot = count($users[0]);
 		} else {
 			return "Can´t connect to the TS server. Pls try again later.";
 		}
-
 		$msg = "##lightgray##::::: Teamspeak Server Info :::::##end##\n\n";
-		$msg .="Get the client:	\"http://www.goteamspeak.com\" \n\n";
+		$msg .="Get the client:	\"https://www.teamspeak.com\" \n\n";
 		$msg .="Note: To connect use ##highlight##".$this->bot->core("settings")->get("Teamspeak","tsdh").":".$this->bot->core("settings")->get("Teamspeak","tssp")."##end## as the connect address\n";
 		$msg .="Server Name: ##highlight##".$this->bot->core("settings")->get("Teamspeak","tssn")."##end##\n";
 		$msg .="Server Address: ##highlight##".$this->bot->core("settings")->get("Teamspeak","tsdh")."##end##\n";
 		$msg .="Server Port: ##highlight##".$this->bot->core("settings")->get("Teamspeak","tssp")."##end##\n\n";
-		$msg .="Number of Players Currently Connected: ##highlight##".$user."##end##\n";
-		$msg .="Server Maximum: ##highlight##".$maxusers."##end##\n";
-		$msg .="Players (Time Connected):\n";
-
-		$player_array = $this -> getTSChannelUsers($this->bot->core("settings")->get("Teamspeak","tsip"), $this->bot->core("settings")->get("Teamspeak","tssp"), $this->bot->core("settings")->get("Teamspeak","tsqp"));
-		$count=count($player_array);
-		if ($count > 1) {
-			$arraycount=1;
-			while ($count > 1) {
-				$player=$player_array[$arraycount][14];
-				$player=str_replace("\"","",$player);
-				$player=ucfirst($player);
-				$time=$player_array[$arraycount][8];
-				$time=$this -> time_convert($time);
-
-				$msg .= '##highlight##'.$player."##end##($time)\n";
-				$count--;
-				$arraycount++;
+		$msg .="Total of channels: ##highlight##".$chtot."##end##\n";
+		$msg .="Number of Players Currently Connected: ##highlight##".$cltot."##end##\n\n";
+		foreach($channels[1] AS $num => $cid) {
+			if($channels[3][$num]>0) {
+				$msg .="##highlight##".str_replace("\s"," ",$channels[2][$num]).":##end##\n";
+				foreach($users[1] AS $id => $uid) {
+					if($cid==$uid) $msg .=" ".$users[2][$id]." ";
+				}
+				$msg .="\n\n";
 			}
-		} else {
-			 $msg .= '##highlight##No Players Connected##end##';
-		}
+		}	
 		return $this -> bot -> core("tools") -> make_blob("Teamspeak server status", '##omni##'.$msg.'##end##');
 	}
-
-	function indexOf($str,$strChar)
-	{
-		 if(strlen(strchr($str,$strChar))>0) {
-				$position_num = strpos($str,$strChar) + strlen($strChar);
-				return $position_num;
-		 } else {
-				return -1;
-		 }
-	}
-
-	function getTSChannelUsers($ip,$port,$tPort)
-	{
-		$uArray = array();
-		$innerArray = array();
-		$out = "";
-		$j = 0;
-		$k = 0;
-
-		$fp = @fsockopen($ip, $tPort, $errno, $errstr, 30);
-		if($fp) {
-				fputs($fp, "pl ".$port."\n");
-				fputs($fp, "quit\n");
-				while(!feof($fp)) {
-					 $out .= fgets($fp, 1024);
-				}
-				$out = str_replace("[TS]", "", $out);
-				$out = str_replace("loginname", "loginname\t", $out);
-				$data = explode("\t", $out);
-				$num = count($data);
-
-				for($i=0;$i<count($data);$i++) {
-					$innerArray[$j] = $data[$i];
-					if($j>=15)
-					{
-						$uArray[$k]=$innerArray;
-						$j = 0;
-						$k = $k+1;
-					} else {
-						$j++;
-					}
-				}
-				fclose($fp);
-		 }
-			return $uArray;
-	}
-
-	function time_convert($time)
-	{
-		$hours = floor($time/3600);
-		$minutes = floor(($time%3600)/60);
-		$seconds = floor(($time%3600)%60);
-
-		if($hours>0) $time = $hours."h ".$minutes."m ".$seconds."s";
-		else if($minutes>0) $time = $minutes."m ".$seconds."s";
-		else $time = $seconds."s";
-
-		return $time;
-	}
+	
 }
 
 ?>
