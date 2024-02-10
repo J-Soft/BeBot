499f66c37bf7d3cf5c4140bd124a0c9269d14882
bitnykk <beatnykk@gmx.fr>
2024-02-10 14:57:42 +0100

    Rendering several CIDB path parameterable with Meathook's default values

--- Core/Aoc/Items.php
+++ Core/Aoc/Items.php
@@ -54,6 +54,10 @@ class Items_Core extends BasePassiveModule
 
         $this -> register_module("items");
 		$this->bot->core("settings")->create("Items", "CIDB", "https://conan.meathooksminions.com", "What is HTTP(s) Central Item Database URL (MeatHooks Minions by default, or your prefered mirror) ?");
+		$this->bot->core("settings")->create("Items", "ItemSubmit", "aoc_items/itemdb_botsubmit.php", "What is CIBD inner path to the Item Submit ; by default from Meathook's API value is : aoc_items/itemdb_botsubmit.php");
+		$this->bot->core("settings")->create("Items", "RecipeSubmit", "aoc_items/itemdb_botrecipesubmit.php", "What is CIBD inner path to the Recipe Submit ; by default from Meathook's API value is : aoc_items/itemdb_botrecipesubmit.php");
+		$this->bot->core("settings")->create("Items", "ItemSearch", "aoc_items/itemdb_botsearch.php", "What is CIBD inner path to the Item Search ; by default from Meathook's API value is : aoc_items/itemdb_botsearch.php");
+		$this->bot->core("settings")->create("Items", "RecipeSearch", "aoc_items/itemdb_botrecipesearch.php", "What is CIBD inner path to the Recipe Search ; by default from Meathook's API value is : aoc_items/itemdb_botrecipesearch.php");
     }
 
     function parse_items($itemText)
@@ -157,7 +161,7 @@ class Items_Core extends BasePassiveModule
 
         $checksum = md5('aocitems' . $salt );
 
-        $url  = $this->bot->core("settings")->get("Items", "CIDB")."/aoc_items/itemdb_botsubmit.php";
+        $url  = $this->bot->core("settings")->get("Items", "CIDB")."/".$this->bot->core("settings")->get("Items", "ItemSubmit");
         $url .= '?lowid='.urlencode($item['lowid']);
         $url .= '&highid='.urlencode($item['highid']);
         $url .= '&lowlvl='.urlencode($item['lowlvl']);
@@ -202,7 +206,7 @@ class Items_Core extends BasePassiveModule
 
         $checksum = md5('aocrecipe' . $salt);
 
-        $url  = $this->bot->core("settings")->get("Items", "CIDB")."/aoc_items/itemdb_botrecipesubmit.php";
+        $url  = $this->bot->core("settings")->get("Items", "CIDB")."/".$this->bot->core("settings")->get("Items", "RecipeSubmit");
         $url .= '?recipelowid='.urlencode($recipeitem['lowid']);
         $url .= '&recipehighid='.urlencode($recipeitem['highid']);
         $url .= '&recipeqty='.urlencode($recipeqty);
@@ -231,7 +235,7 @@ class Items_Core extends BasePassiveModule
         $salt       = $passkey."_".$botname;
         $checksum   = md5('aocitems' . $salt );
         
-        $url  = $this->bot->core("settings")->get("Items", "CIDB")."/aoc_items/itemdb_botsearch.php";
+        $url  = $this->bot->core("settings")->get("Items", "CIDB")."/".$this->bot->core("settings")->get("Items", "ItemSearch");
         $url .= '?search='.urlencode($words);
         $url .= '&botname='.urlencode($this->bot->botname);
         $url .= '&pre='.urlencode($this -> bot -> commpre);
@@ -262,7 +266,7 @@ class Items_Core extends BasePassiveModule
         $salt       = $passkey."_".$botname;
         $checksum   = md5('aocrecipe' . $salt );
         
-        $url  = $this->bot->core("settings")->get("Items", "CIDB")."/aoc_items/itemdb_botrecipesearch.php";
+        $url  = $this->bot->core("settings")->get("Items", "CIDB")."/".$this->bot->core("settings")->get("Items", "RecipeSearch");
         $url .= '?search='.urlencode($words);
         $url .= '&botname='.urlencode($this->bot->botname);
         $url .= '&pre='.urlencode($this -> bot -> commpre);
